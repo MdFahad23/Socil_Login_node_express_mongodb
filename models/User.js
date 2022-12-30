99b829107b646b647e17080d743931d7e056116f
Md Fahad <fahadbdctglh8@gmail.com>
2022-12-30 15:48:22 +0600

    Create User Models

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const UserSchema = Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 150,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minLength: 8,
+      maxLength: 150,
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.methods.generateJwt = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+    },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: "24d" }
+  );
+  return token;
+};
+
+module.exports.User = model("User", UserSchema);
